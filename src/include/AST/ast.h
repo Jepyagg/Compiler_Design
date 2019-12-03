b4b25beffbb7c440b693905137a9d2e27b7b390b
iamch15542 <kuso1112.cs06@nctu.edu.tw>
2019-12-04 01:35:00 +0800

    Finish All node && pass declaration expression simple invocation

--- src/include/AST/ast.h
+++ src/include/AST/ast.h
@@ -73,7 +73,11 @@ class AstNode {
 
 class Expression_Node : public AstNode {
     public:
+        Expression_Node(){};
         virtual void accept(class VisitorBase &v) = 0;
+        virtual ~Expression_Node(){
+            // cout <<"expr\n";
+        };
 };
 
 class Array_Node : public AstNode {
@@ -100,135 +104,12 @@ class Const_Node : public Expression_Node { //save type and const
             free(str);
             if(arr_list != NULL) {
                 for(auto tmp : *arr_list) {
-                    cout <<"arr_list\n";
+                    // cout <<"arr_list\n";
                     delete tmp;
                 }
                 delete arr_list;
             }
         }
-};
-
-class Binary_Operator_Node : public Expression_Node {
-    public:
-        char* oper;
-        int line_num, col_num;
-        Expression_Node *leftoperand, *rightoperand;
-        Binary_Operator_Node(Expression_Node *left, Expression_Node *right, char *opt, int line, int col):leftoperand(left), rightoperand(right), oper(opt), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Unary_Operator_Node : public Expression_Node {
-    public:
-        char* oper;
-        int line_num, col_num;
-        Expression_Node *operand;
-        Unary_Operator_Node(Expression_Node *opd, char *opt, int line, int col):operand(opd), oper(opt), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-
-class Variable_Reference_Node : public Expression_Node {
-    public:
-        int line_num, col_num;
-        Id_Node* ident;
-        vector<Expression_Node*>* expr_list;
-        Variable_Reference_Node(Id_Node *name, vector<Expression_Node*> *ind, int line, int col):ident(name), expr_list(ind), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Function_Call_expr_Node : public Expression_Node {
-    public:
-        int line_num, col_num;
-        Id_Node* ident;
-        vector<Expression_Node*>* expr_list;
-        Function_Call_expr_Node(Id_Node* id, vector<Expression_Node*> *expression, int line, int col):ident(id), expr_list(expression), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Statement_Node : public AstNode {
-    public:
-        virtual void accept(class VisitorBase &v) = 0;
-};
-
-class Compound_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        vector<Declaration_Node *>* decl_list;
-        vector<Statement_Node *>* stat_list;
-        Compound_Node(vector<Declaration_Node *> *decl, vector<Statement_Node *> *stat, int line, int col):decl_list(decl), stat_list(stat), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Assignment_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Variable_Reference_Node *var;
-        Expression_Node *expr;
-        Assignment_Node(Variable_Reference_Node *value, Expression_Node *expression, int line, int col):var(value), expr(expression), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Print_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Expression_Node *expr;
-        Print_Node(Expression_Node *expr, int line, int col):expr(expr), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Read_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Variable_Reference_Node *var;
-        Read_Node(Variable_Reference_Node *value, int line, int col):var(value), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class If_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Expression_Node *expr;
-        vector<Statement_Node *>* stat_list;
-        vector<Statement_Node *>* stat2_list;
-        If_Node(Expression_Node *expression, vector<Statement_Node *>* stat, vector<Statement_Node *>* stat2, int line, int col):expr(expression), stat_list(stat), stat2_list(stat2), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class While_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Expression_Node *expr;
-        vector<Statement_Node *>* stat_list;
-        While_Node(Expression_Node *expression, vector<Statement_Node *>* stat, int line, int col):expr(expression), stat_list(stat), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class For_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Id_Node* ident;
-        Assignment_Node* val;
-        Const_Node* val2, *val3;
-        vector<Statement_Node *>* stat_list;
-        For_Node(Id_Node *id, Assignment_Node* const1, Const_Node* const2, Const_Node* const3, vector<Statement_Node *>* stat, int line, int col):ident(id), val(const1), val2(const2), val3(const3), stat_list(stat), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Return_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Expression_Node *expr;
-        Return_Node(Expression_Node *expression, int line, int col):expr(expression), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-};
-
-class Function_Call_Node : public Statement_Node {
-    public:
-        int line_num, col_num;
-        Id_Node* ident;
-        vector<Expression_Node*>* expr_list;
-        Function_Call_Node(Id_Node* id, vector<Expression_Node*> *expression, int line, int col):ident(id), expr_list(expression), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
 };
 
 class Id_Node : public AstNode {
@@ -240,6 +121,255 @@ class Id_Node : public AstNode {
         void accept(VisitorBase &v) { v.visit(this); }
         ~Id_Node() {
             free(identifier);
+        }
+};
+
+class Declaration_Node : public AstNode {
+    public:
+        int line_num, col_num;
+        vector<Id_Node *> * id_list = NULL;
+        Const_Node* type_val;
+        Declaration_Node(vector<Id_Node *> * list, Const_Node* type, int line, int col) : id_list(list), type_val(type), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Declaration_Node() {
+            // cout << "Declaration_Node\n";
+            delete type_val;
+            if(id_list != NULL) {
+                for(auto tmp : *id_list) {
+                    delete tmp;
+                }
+                delete id_list;
+            }
+        }
+};
+
+class Binary_Operator_Node : public Expression_Node {
+    public:
+        char* oper;
+        int line_num, col_num;
+        Expression_Node *leftoperand, *rightoperand;
+        Binary_Operator_Node(Expression_Node *left, Expression_Node *right, char *opt, int line, int col):leftoperand(left), rightoperand(right), oper(opt), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Binary_Operator_Node() {
+            // cout <<"Binary_Operator_Node\n";
+            free(oper);
+            delete leftoperand;
+            delete rightoperand;
+        }
+};
+
+class Unary_Operator_Node : public Expression_Node {
+    public:
+        char* oper;
+        int line_num, col_num;
+        Expression_Node *operand;
+        Unary_Operator_Node(Expression_Node *opd, char *opt, int line, int col):operand(opd), oper(opt), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Unary_Operator_Node() {
+            // cout << "Unary_Operator_Node\n";
+            free(oper);
+            delete operand;
+        }
+};
+
+
+class Variable_Reference_Node : public Expression_Node {
+    public:
+        int line_num, col_num;
+        Id_Node* ident;
+        vector<Expression_Node*>* expr_list = NULL;
+        Variable_Reference_Node(Id_Node *name, vector<Expression_Node*> *ind, int line, int col):ident(name), expr_list(ind), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Variable_Reference_Node() {
+            delete ident;
+            if(expr_list != NULL) {
+                for(auto tmp : *expr_list) {
+                    delete tmp;
+                }
+                delete expr_list;
+            }
+        }
+};
+
+class Function_Call_expr_Node : public Expression_Node {
+    public:
+        int line_num, col_num;
+        Id_Node* ident;
+        vector<Expression_Node*>* expr_list = NULL;
+        Function_Call_expr_Node(Id_Node* id, vector<Expression_Node*> *expression, int line, int col):ident(id), expr_list(expression), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Function_Call_expr_Node() {
+            delete ident;
+            if(expr_list != NULL) {
+                for(auto tmp : *expr_list) {
+                    delete tmp;
+                }
+                delete expr_list;
+            }
+        }
+};
+
+class Statement_Node : public AstNode {
+    public:
+        Statement_Node(){};
+        virtual void accept(class VisitorBase &v) = 0;
+        virtual ~Statement_Node(){
+            // cout << "stat\n";
+        };
+};
+
+class Compound_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        vector<Declaration_Node *>* decl_list = NULL;
+        vector<Statement_Node *>* stat_list = NULL;
+        Compound_Node(vector<Declaration_Node *> *decl, vector<Statement_Node *> *stat, int line, int col):decl_list(decl), stat_list(stat), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Compound_Node() {
+            // cout << "Compound\n";
+            if(decl_list != NULL) {
+                for(auto tmp : *decl_list) {
+                    delete tmp;
+                }
+                delete decl_list;
+            }
+            if(stat_list != NULL) {
+                for(auto tmp : *stat_list) {
+                    delete tmp;
+                }
+                delete stat_list;
+            }
+        }
+};
+
+class Assignment_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Variable_Reference_Node *var;
+        Expression_Node *expr;
+        Assignment_Node(Variable_Reference_Node *value, Expression_Node *expression, int line, int col):var(value), expr(expression), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Assignment_Node() {
+            delete var;
+            delete expr;
+        }
+};
+
+class Print_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Expression_Node *expr;
+        Print_Node(Expression_Node *expr, int line, int col):expr(expr), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Print_Node() {
+            cout <<"Print_Node\n";
+            delete expr;
+        }
+};
+
+class Read_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Variable_Reference_Node *var;
+        Read_Node(Variable_Reference_Node *value, int line, int col):var(value), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Read_Node() {
+            delete var;
+        }
+};
+
+class If_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Expression_Node *expr;
+        vector<Statement_Node *>* stat_list = NULL;
+        vector<Statement_Node *>* stat2_list = NULL;
+        If_Node(Expression_Node *expression, vector<Statement_Node *>* stat, vector<Statement_Node *>* stat2, int line, int col):expr(expression), stat_list(stat), stat2_list(stat2), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~If_Node() {
+            delete expr;
+            if(stat_list != NULL) {
+                for(auto tmp : *stat_list) {
+                    delete tmp;
+                }
+                delete stat_list;
+            }
+            if(stat2_list != NULL) {
+                for(auto tmp : *stat2_list) {
+                    delete tmp;
+                }
+                delete stat2_list;
+            }
+        }
+};
+
+class While_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Expression_Node *expr;
+        vector<Statement_Node *>* stat_list = NULL;
+        While_Node(Expression_Node *expression, vector<Statement_Node *>* stat, int line, int col):expr(expression), stat_list(stat), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~While_Node() {
+            delete expr;
+            if(stat_list != NULL) {
+                for(auto tmp : *stat_list) {
+                    delete tmp;
+                }
+                delete stat_list;
+            }
+        }
+};
+
+class For_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Id_Node* ident;
+        Assignment_Node* val;
+        Const_Node* val2, *val3;
+        vector<Statement_Node *>* stat_list = NULL;
+        For_Node(Id_Node *id, Assignment_Node* const1, Const_Node* const2, Const_Node* const3, vector<Statement_Node *>* stat, int line, int col):ident(id), val(const1), val2(const2), val3(const3), stat_list(stat), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~For_Node() {
+            delete ident;
+            delete val;
+            delete val2;
+            delete val3;
+            if(stat_list != NULL) {
+                for(auto tmp : *stat_list) {
+                    delete tmp;
+                }
+                delete stat_list;
+            }
+        }
+};
+
+class Return_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Expression_Node *expr;
+        Return_Node(Expression_Node *expression, int line, int col):expr(expression), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Return_Node() {
+            delete expr;
+        }
+};
+
+class Function_Call_Node : public Statement_Node {
+    public:
+        int line_num, col_num;
+        Id_Node* ident;
+        vector<Expression_Node*>* expr_list = NULL;
+        Function_Call_Node(Id_Node* id, vector<Expression_Node*> *expression, int line, int col):ident(id), expr_list(expression), line_num(line), col_num(col){};
+        void accept(VisitorBase &v) { v.visit(this); }
+        ~Function_Call_Node() {
+            delete ident;
+            if(expr_list != NULL) {
+                for(auto tmp : *expr_list) {
+                    delete tmp;
+                }
+                delete expr_list;
+            }
         }
 };
 
@@ -285,29 +415,10 @@ class Function_Node : public AstNode {
         }
 };
 
-class Declaration_Node : public AstNode {
-    public:
-        int line_num, col_num;
-        vector<Id_Node *> * id_list = NULL;
-        Const_Node* type_val;
-        Declaration_Node(vector<Id_Node *> * list, Const_Node* type, int line, int col) : id_list(list), type_val(type), line_num(line), col_num(col){};
-        void accept(VisitorBase &v) { v.visit(this); }
-        ~Declaration_Node() {
-            // cout << "Declaration_Node\n";
-            delete type_val;
-            if(id_list != NULL) {
-                for(auto tmp : *id_list) {
-                    delete tmp;
-                }
-                delete id_list;
-            }
-        }
-};
-
 class Program_body : public AstNode {
     public:
         int line_num, col_num;
-        vector<Declaration_Node *>* decl_list = NULL;
+        vector<Declaration_Node *>* decl_list;
         vector<Function_Node *>* func_list = NULL;
         Compound_Node* comp;
         Program_body(vector<Declaration_Node *> *decl, vector<Function_Node *>* func, Compound_Node* com):decl_list(decl), func_list(func), comp(com){};
